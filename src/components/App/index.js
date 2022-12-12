b7a721de9479660bfa1a5810f10a173ca56783cd
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2022-12-12 09:25:32 +0000

    created and rendered the first component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,16 @@
+import BlogPost from "../BlogPost";
+
 function App() {
-  return null;
+  return (
+    <BlogPost
+      title= "My First Blog"
+      author= "Ashraf Rahman"
+      datePosted= "20/11/2019"
+      content="This is my attempt at building a React blog, wish me luck"
+      imageSrc="https://images.pexels.com/photos/1181472/pexels-photo-1181472.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+      imageAlt="A couple of coders."
+    />
+  )
 }
 
 export default App;
--- src/components/BlogPost/index.js
+++ src/components/BlogPost/index.js
@@ -1,5 +1,14 @@
-function BlogPost() {
-  return null;
+import React from 'react';
+
+function BlogPost(props){
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <p>By {props.author} on {props.datePosted}</p>
+      <p>{props.content}</p>
+      <img src={props.imageSrc} alt={props.imageAlt}/>
+    </div>
+  )
 }
 
-export default BlogPost;
+export default BlogPost
